3a0a7f2501e92bf6446cf0cad2e7b3900b0997ea
Diogo Vianna V. Araujo <diogoviannaaraujo@gmail.com>
2024-09-13 19:57:23 -0300

    Initial Commit

---
+++ Chonchon/ChonchonApp.swift
@@ -0,0 +1,17 @@
+//
+//  ChonchonApp.swift
+//  Chonchon
+//
+//  Created by Diogo Vianna V. Araújo on 13/9/24.
+//
+
+import SwiftUI
+
+@main
+struct ChonchonApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ Chonchon/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  Chonchon
+//
+//  Created by Diogo Vianna V. Araújo on 13/9/24.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
